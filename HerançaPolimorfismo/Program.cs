f3f538242550445cefacf6bcf891ec637fa04487
Matheus <Matheus@DESKTOP-GUELB60>
2025-04-10 23:33:48 -0300

    Add project files.

---
+++ HerançaPolimorfismo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Herança.Entities;
+
+namespace HerançaPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brow ", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
