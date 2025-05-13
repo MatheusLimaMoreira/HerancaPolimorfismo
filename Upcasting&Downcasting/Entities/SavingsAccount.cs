4393e7f696c636c2cb9ffef9610061469f33a413
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-13 18:04:43 -0300

    Criação do SavingsAccount(Upcasting&Downcasting)

---
+++ Upcasting&Downcasting/Entities/SavingsAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upcasting_Downcasting.Entities
+{
+    internal class SavingsAccount
+    {
+    }
+}
---
+++ Upcasting&Downcasting/Program.cs
@@ -0,0 +1,14 @@
+﻿using Upcasting_Downcasting.Entities;
+
+namespace HerançaPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
