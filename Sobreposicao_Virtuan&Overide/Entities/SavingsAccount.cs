39daf4ebc60496f2a474569e917971c61853a933
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-15 11:15:56 -0300

    Usando sobreposição com virtual e override

---
+++ Sobreposicao_Virtuan&Overide/Entities/SavingsAccount.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobreposicao_Virtuan_Overide.Entities
+{
+    internal class SavingsAccount : Account
+
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        /*public override void withdraw(double amount) 
+        {
+            Balance -= amount;
+        }
+        */
+
+        public override void withdraw(double amount)
+        {
+            base.withdraw(amount);
+            Balance -= 2.0; // Overriding the base class method to add an additional fee
+        }
+    }
+}
---
+++ Sobreposicao_Virtuan&Overide/Program.cs
@@ -0,0 +1,20 @@
+﻿using Sobreposicao_Virtuan_Overide.Entities;
+
+namespace Sobreposicao_Virtuan_Overide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+            Account acc3 = new SavingsAccount(1003, "Bob", 500.0, 0.01);
+
+            acc1.withdraw(10.0);
+            acc2.withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
