166b463e8ffedd8e9f7246126cb8deb52107ecc9
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-16 10:45:49 -0300

    Exercicio de Fixacao

---
+++ ExercicioResolvido/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioResolvido.Entities
+{
+    internal class Employee
+    {
+        public string? Name { get; set; }
+        public double Houers { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+        public Employee(string name, double houers, double valuePerHour)
+        {
+            Name = name;
+            Houers = houers;
+            ValuePerHour = valuePerHour;
+        }
+        public virtual double Payment()
+        {
+            return Houers * ValuePerHour;
+        }
+    }    
+}
---
+++ ExercicioResolvido/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioResolvido.Entities;
+
+namespace ExercicioResolvido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Entities.Employee> list = new List<Entities.Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                double hours = double.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+
+                if (ch == 'y') // adicionando os dados na lista.
+                {
+                    Console.Write("Additional charge: ");
+                    double addicionalCharge = double.Parse(Console.ReadLine());
+                    list.Add(new OutsourcedEmployee
+                        (name, hours, valuePerHour, addicionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2"));
+            }
+        }
+    }
+}
