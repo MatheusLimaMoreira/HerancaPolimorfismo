a83ea5389188ffdc85874fb912f08a93eacd60b4
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-16 12:08:59 -0300

    Execicio de fixacao

---
+++ ExercicioFixacao/Entities/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateOnly ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateOnly manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public override string PriceTag()
+        {
+            return Name + " (used) $ " + Price.ToString("F2") 
+                + " (Manufacture date: " 
+                + ManufactureDate.ToString("dd/MM/yyyy") + ")";
+        }
+    } 
+}
