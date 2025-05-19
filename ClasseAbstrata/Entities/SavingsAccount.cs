a91bdeca7a44e45839b68eeaf89e6e9f4e41bada
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-19 10:59:15 -0300

    Implementacao de classes abstratas

---
+++ ClasseAbstrata/Entities/SavingsAccount.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseAbstrata.Entities
+{
+    sealed class SavingsAccount : Account // Evita que a classe seja herdada.
+
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        /*public override void withdraw(double amount) 
+        {
+            Balance -= amount;
+        }
+        */
+
+        public sealed override void withdraw(double amount) // Evita que o metodo seja sobrescrito em uma subclasse.
+        {
+            base.withdraw(amount);
+            Balance -= 2.0; // Overriding the base class method to add an additional fee
+        }
+    }
+}
+ 
--- Sobreposicao_Virtuan&Overide/Program.cs
+++ Sobreposicao_Virtuan&Overide/Program.cs
@@ -8,7 +8,7 @@ namespace Sobreposicao_Virtuan_Overide
         {
             Account acc1 = new Account(1001, "Alex", 500.0);
             Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
-            Account acc3 = new SavingsAccount(1003, "Bob", 500.0, 0.01);
+            
 
             acc1.withdraw(10.0);
             acc2.withdraw(10.0);
