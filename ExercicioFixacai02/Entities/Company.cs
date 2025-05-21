4b0c83d08c19ef462bbd33a0b0216dc91f343224
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-21 10:43:40 -0300

    Exercicio de fixação 02

---
+++ ExercicioFixacai02/Entities/Company.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioFixacao02.Entities;
+
+namespace ExercicioFixacao02.Entities
+{
+    internal class Company : TaxPayer
+    {
+        public int NumberofEmployees { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(string name, double anualIncome, int numberofEmployees)
+            : base(name, anualIncome)
+        {
+            NumberofEmployees = numberofEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberofEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
--- ExercicioFixacai02/Program.cs
+++ ExercicioFixacai02/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace ExercicioFixacai02
+﻿using ExercicioFixacao02.Entities;
+
+namespace ExercicioFixacai02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.WriteLine("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data: ");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine());
+
+                if (ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberofEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberofEmployees));
+                }
+            }
+
+            Console.WriteLine("TAXES PAID: ");
+            foreach (TaxPayer taxPayer in list)
+            {
+                Console.WriteLine(taxPayer.Name + ": $ " + taxPayer.Tax().ToString("F2"));
+            }
+
+            double sum = 0.0;
+            foreach (TaxPayer taxPayer in list)
+            {
+                sum += taxPayer.Tax();
+            }
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2"));
         }
     }
 }
